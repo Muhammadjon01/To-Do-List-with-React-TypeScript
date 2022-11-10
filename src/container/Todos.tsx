5a26c732d8734785caa1d4acaddebeed309d37c1
mc0der <gmuhammadjon01@gmail.com>
2022-11-10 17:29:37 +0500

    add Todos page

---
+++ src/container/Todos.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Todos: React.FC = () => {
+    return (
+        <div className="w-full h-screen bg-gradient-to-r from-green-400 to-blue-500">
+          <div className="w-[80%] h-screen bg-white-600  m-auto">
+            <div className="lg:w-[50%] ms:m-auto mx-auto">
+              <h1 className="text-4xl font-bold py-10 text-center text-white">
+                To Do list
+              </h1>
+            </div>
+            <div className="flex justify-between">
+              <input
+                className="w-full rounded"
+                type="text"
+                // value={text}
+                // onChange={handleChange}
+                placeholder="Add your task"
+              />
+              <button
+                className="rounded text-white bg-cyan-500 hover:bg-cyan-600 p-2"
+                // onClick={addTodo}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          
+        </div>
+      );
+}
