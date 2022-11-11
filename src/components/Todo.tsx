d4d94c4646bdf3a3c81934114534014b0b8e8c86
mc0der <gmuhammadjon01@gmail.com>
2022-11-11 15:22:57 +0500

    fix tailwind

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import {ITodos} from "../interface"
+import React from "react";
+import { ITodos } from "../interface";
+import {AiFillDelete} from "react-icons/ai"
+import {IoCheckmarkDoneCircle} from "react-icons/io5"
 
 // type TodoProps = {
 //     todos: ITodos[];
 //     deleteTodo:(id: number) => void;
 // }
 
-export const Todo: React.FC<{text: string, isCompleted:boolean, deleteTodo: any, completeTodo: any }> = ({text, isCompleted, deleteTodo, completeTodo}): JSX.Element =>{
-    return (
-        <div className="w-full flex flex-col rounded">
-            <div className="w-full text-start flex flex-wrap justify-between border-b-2">
-                <ul className="pt-4">
-                    <li style={isCompleted ? {textDecoration: "line-through"} : {}}>
-                        {text}
-                    </li>
-                </ul>
-                <div className="space-x-2 mt-4 items-end text-white">
-                    <button onClick={completeTodo}>Complete</button>
-                    <button>Edit</button>
-                    <button onClick={deleteTodo}>Delete</button>
-                </div>
-            </div>
+export const Todo: React.FC<{
+  text: string;
+  isCompleted: boolean;
+  deleteTodo: any;
+  completeTodo: any;
+}> = ({ text, isCompleted, deleteTodo, completeTodo }): JSX.Element => {
+  return (
+    <div className="w-full flex flex-col rounded">
+      <div className="w-full text-start flex flex-wrap justify-between border-b-2">
+        <ul className="pt-4">
+          <li style={isCompleted ? { textDecoration: "line-through" } : {}}>
+            {text}
+          </li>
+        </ul>
+        <div className="space-x-2 mt-4 items-end text-white">
+          <button className="text-green-500" onClick={completeTodo}><IoCheckmarkDoneCircle size={30}/></button>
+          <button className="text-red-400" onClick={deleteTodo}><AiFillDelete size={30} /></button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/container/Todos.tsx
+++ src/container/Todos.tsx
@@ -25,15 +25,15 @@ export const Todos: React.FC = () => {
 
   const completeTodo = (id: number) => {
     let updateTodo = todos.map((elem) => {
-      if(elem.id == id) {
+      if (elem.id == id) {
         elem.isCompleted = !elem.isCompleted;
         return elem;
       }
       return elem;
-    })
+    });
 
-    setTodos(updateTodo)
-  }
+    setTodos(updateTodo);
+  };
 
   const deleteTodo = (id: number) => {
     let filterTodos = todos.filter((elem) => elem.id !== id);
@@ -42,7 +42,7 @@ export const Todos: React.FC = () => {
 
   return (
     <div className="w-full h-screen bg-gradient-to-r from-green-400 to-blue-500">
-      <div className="w-[80%] h-screen bg-white-600  m-auto">
+      <div className="w-[60%] h-screen bg-white-600  m-auto">
         <div className="lg:w-[50%] ms:m-auto mx-auto">
           <h1 className="text-4xl font-bold py-10 text-center text-white">
             To Do list
@@ -50,7 +50,7 @@ export const Todos: React.FC = () => {
         </div>
         <div className="flex justify-between">
           <input
-            className="w-full rounded"
+            className="w-full rounded outline-none px-4"
             type="text"
             value={text}
             onChange={handleChange}
@@ -63,22 +63,24 @@ export const Todos: React.FC = () => {
             Add
           </button>
         </div>
-      </div>
-      <div className="lg:w-[50%]  bg-white rounded mx-auto py-5">
-        <div className="bg-white space-y-2 md:mx-auto mx-auto my-4 px-4">
-          <h1 className="text-4xl font-bold border-b-2">Here is your task!</h1>
-          {todos.length > 0 &&
-            todos.map((elem) => {
-              return (
-                <Todo
-                  key={elem.id}
-                  text={elem.text}
-                  isCompleted = {elem.isCompleted}
-                  deleteTodo={() => deleteTodo(elem.id)}
-                  completeTodo = {() => completeTodo(elem.id)}
-                />
-              );
-            })}
+        <div className="lg:w-[50%]  bg-white rounded mx-auto py-5 my-10">
+          <div className="bg-white space-y-2 md:mx-auto mx-auto my-4 px-4">
+            <h1 className="text-4xl text-red-400 font-bold border-b-2">
+              Here is your task!
+            </h1>
+            {todos.length > 0 &&
+              todos.map((elem) => {
+                return (
+                  <Todo
+                    key={elem.id}
+                    text={elem.text}
+                    isCompleted={elem.isCompleted}
+                    deleteTodo={() => deleteTodo(elem.id)}
+                    completeTodo={() => completeTodo(elem.id)}
+                  />
+                );
+              })}
+          </div>
         </div>
       </div>
     </div>
